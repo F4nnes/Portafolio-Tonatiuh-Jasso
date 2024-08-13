27df358ea7ec7846bb19b26fc004865cf97efdb8
F4nnes <tonatiuh_06flores@live.com>
2024-08-13 08:54:15 -0600

    "agregado de seccion para errores" "inicio de validacion de formulario" "cambio de archivo js a module"

---
+++ js/customError.js
@@ -0,0 +1,25 @@
+
+
+
+export const tiposError = [
+    "valueMissing", "typeMissmatch",
+    "patternMissmatch", "tooShort"
+]
+
+export const mensajes = {
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacío.",
+        patternMismatch: "Por favor, ingrese un nombre válido.",
+        tooShort: "Por favor, ingrese un nombre válido.",
+    },
+    email: {
+        valueMissing: "El campo email no puede estar vacío.",
+        typeMismatch: "Por favor, ingrese un email válido.",
+        tooShort: "Por favor, ingrese un e-mail válido.",
+    },
+    asunto: {
+        valueMissing: "El campo identificación no puede estar vacío.",
+        patternMismatch: "Por favor, ingrese un titulo sobre su asunto.",
+        tooShort: "El campo no tiene caracteres suficientes.",
+    },
+};
