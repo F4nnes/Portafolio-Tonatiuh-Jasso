2b504b33b7a9602f7fc13df8889d1c97df5dcd28
F4nnes <tonatiuh_06flores@live.com>
2024-08-12 22:01:18 -0600

    "adicion de comentarios en funcion de cambio de color"

--- js/main.js
+++ js/main.js
@@ -5,9 +5,12 @@ const itemHeader = document.querySelectorAll('.menu_list_item');
 
 
 
+// Cambio de color en items de barra de navegacion
 itemHeader.forEach((item)=>{
     item.addEventListener('click', () => {
+        // Agrega la clase 'active' al elemento que se hizo click
         item.classList.add('active');
+        // Elimina la clase 'active' de los demás elementos
         itemHeader.forEach((otherItem)=>{
             if (otherItem !== item) {
                 otherItem.classList.remove('active');
