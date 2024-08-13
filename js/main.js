94f76e195859a730ac64b1809fb3fc15db63757a
F4nnes <tonatiuh_06flores@live.com>
2024-08-12 21:59:59 -0600

    "agregado de funcion para el cambio de color de items en barra de navegacion"

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
-// Union de funciones
+// Constantes
 
+
+const itemHeader = document.querySelectorAll('.menu_list_item');
+
+
+
+itemHeader.forEach((item)=>{
+    item.addEventListener('click', () => {
+        item.classList.add('active');
+        itemHeader.forEach((otherItem)=>{
+            if (otherItem !== item) {
+                otherItem.classList.remove('active');
+            }
+        });
+    });
+});
